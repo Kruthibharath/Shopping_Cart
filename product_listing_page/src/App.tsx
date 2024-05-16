46386eed1c41dd04509624cf19958fa3da7cdfba
Kruthibharath <kruthi.bharath@gmail.com>
2024-05-16 16:18:30 +0100

    Initial project set up with some dependencies and creating routes for navigation

---
+++ product_listing_page/src/App.tsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+
+function App() {
+  return (
+    <Container>
+      hello
+      <Routes>
+        <Route path="/"></Route>
+      </Routes>
+    </Container>
+  );
+}
+
+export default App;
